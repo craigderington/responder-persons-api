650b2a22b50a415d8eca5192291d174c1c1f828c
Craig Derington <craig@craigderington.me>
2019-04-03 16:57:30 -0400

    Add events to models.py.

--- models.py
+++ models.py
@@ -27,18 +27,18 @@ class Group(Model):
     group_name = fields.TextField()
     active = fields.BooleanField(default=True)
 
+    class Meta:
+        table = 'group'
+
     def __str__(self):
         if self.group_name:
             return f'{self.group_name}'
         return self.id
 
-    class Meta:
-        table = 'group'
-
 
 class Person(Model):
     """
-    A small class for our database
+    Person class instance
     """
     id = fields.IntField(pk=True)
     first_name = fields.TextField()
@@ -51,3 +51,42 @@ class Person(Model):
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
+
+
+class MeetUp(Model):
+    """
+    Meetup class instance
+    """
+
+    id = fields.IntField(pk=True)
+    name = fields.CharField(max_length=255)
+    status = fields.BooleanField(default=True)
+
+    class Meta:
+        table = 'meetup'
+
+    def __str__(self):
+        if self.name:
+            return f"{self.name} MeetUp"
+        return self.id
+
+
+class Event(Model):
+    """
+    Event class instance
+    """
+
+    id = fields.IntField(pk=True)
+    name = fields.CharField(max_length=255)
+    event_date = fields.DatetimeField()
+    event_last_updated = fields.DatetimeField(auto_now=True)
+    meetup = fields.ForeignKeyField('models.MeetUp', related_name='events')
+    participants = fields.ManyToManyField('models.Group', related_name='events', through='event_group')
+
+    class Meta:
+        table = 'event'
+
+    def __str__(self):
+        if self.name and self.event_date:
+            return f"{self.event_name} {self.event_date}"
+        return self.id
